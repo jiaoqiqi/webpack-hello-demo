51b636b466928ff49c9aac1abc3d89cac21629d0
jiaoqiqi <1176932165@qq.com>
2017-07-15 18:55:13 +0800

    select user info
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

---
+++ server/userSelect.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let router = express.Router();
+
+let db = require('./connection');
+
+let userSQL = require('./userSql');
+
+router.get('/userSelect', (req, res) => {
+    db.query(userSQL.insert, [req.body.id,req.body.name, req.body.sex, req.body.age,
+        req.body.tel, req.body.mail, req.body.note], function (err, result) {
+        if (err) return err;
+
+        res.send('User added to database with ID: ' + result.insertId);
+    });
+});
+
+
+module.exports = router;

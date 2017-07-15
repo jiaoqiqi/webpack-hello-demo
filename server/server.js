e4bf23dcda23556ee20aefdf60d6c63467d06ba3
jiaoqiqi <1176932165@qq.com>
2017-07-15 18:06:35 +0800

    use postman text update success
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- server/server.js
+++ server/server.js
@@ -7,8 +7,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/'));
+
 const userInsert = require("./userInsert");
-app.use('/',userInsert);
+const userUpdate = require("./userUpdate");
+app.use('/',userUpdate);
+
 app.listen(3000, () => {
     console.log('server start, listening is 3000');
 });

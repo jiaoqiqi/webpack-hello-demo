c275eb7fccbb16448332633c32b9bb443380c08c
jiaoqiqi <1176932165@qq.com>
2017-07-15 21:16:19 +0800

    try display info to page
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = new express;
-// const db = require("./connection.js");
+const db = require("./connection.js");
 const  bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
@@ -14,7 +14,22 @@ const userSelect = require("./userSelect");
 const userDelete = require("./userDelete");
 app.use('/',userInsert,userUpdate,userSelect,userDelete);
 
-app.listen(3000, () => {
+
+const selectSQL = 'select * from user';
+const arr = [];
+db.query(selectSQL, function(err, rows) {
+    if (err) throw err;
+    for (var i = 0; i < rows.length; i++) {
+        arr[i] = rows[i].name;
+    }
+    //把搜索值输出
+    app.get('/', function (req, res) {
+        res.send(arr);
+    });
+});
+
+
+    app.listen(3000, () => {
     console.log('server start, listening is 3000');
 });
 

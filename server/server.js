1ca454e68d806ea4dd219e973c87ed676afd3ac6
jiaoqiqi <1176932165@qq.com>
2017-07-15 13:26:34 +0800

    use sql and server start success
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = new express;
+const db = require("./connection.js");
+// const  bodyParser = require("body-parser");
+//
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/'));
+
+app.listen(3000, () => {
+    console.log('server start, listening is 3000');
+});
+
+module.exports = app;

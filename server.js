8bf20cd5409426574b0cd05a8a10a0a17e96b02e
jiaoqiqi <1176932165@qq.com>
2017-07-16 21:15:13 +0800

    change file constructor
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = new express;
-const db = require("./connection.js");
 const  bodyParser = require("body-parser");
 const path = require('path');
 
@@ -10,28 +9,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/'));
 
-const userInsert = require("./userInsert");
-const userUpdate = require("./userUpdate");
-const userSelect = require("./userSelect");
-const userDelete = require("./userDelete");
+const userInsert = require("./server/routers/userInsert");
+const userUpdate = require("./server/routers/userUpdate");
+const userSelect = require("./server/routers/userSelect");
+const userDelete = require("./server/routers/userDelete");
 app.use('/',userInsert,userUpdate,userSelect,userDelete);
 
 
-// const selectSQL = 'select * from user';
-// const arr = [];
-// db.query(selectSQL, function(err, rows) {
-//     if (err) throw err;
-//     for (var i = 0; i < rows.length; i++) {
-//         arr[i] = rows[i].name;
-//     }
-//     //把搜索值输出
-//     app.get('/', function (req, res) {
-//         res.send(arr);
-//     });
-// });
-
 app.get('/', function (req, res) {
-    res.sendFile(path.resolve("./index.html"));
+    res.sendFile(path.resolve("./","index.html"));
 });
 
 
